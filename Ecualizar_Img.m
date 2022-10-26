ab0995c3b0280b00e78afea3dd2059cccf53d11a
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-10-26 18:35:06 -0500

    Fin del codigo

--- Ecualizar_Img.m
+++ Ecualizar_Img.m
@@ -2,20 +2,33 @@ clc;
 clear all;
 close all;
 
-%Abrir la imagen
-img=imread('IMG\1.jpg');
+%Extraccion de las imagenes
+ruta={'IMG/1.jpg','IMG/2.jpg','IMG/3.jpg','IMG/4.jpg'};
 
-%Ecualizacion de la imagen
-imgec=histeq(img,10);%Número de niveles discretos de gris
+for k=1:4
+    %Abrir la imagen
+    img=imread(string(ruta(k)));
 
-%Mostramos la imagen
-figure()
-imshowpair(img,imgec,'montage')
+    %Interfaz para pedir los niveles discretos de gris
+    prompt="Ingrese el valor del nivel discreto de gris";
+    t='Valor del nivel de gris';
+    dims=[1 50];
+    definput={'20'};
+    n=inputdlg(prompt,t,dims,definput);
+    n=str2double(n);
 
-figure()
-subplot(1,2,1)
-imhist(img)
-title('Histograma original')
-subplot(1,2,2)
-imhist(imgec)
-title('Histograma Ecualizado')
+    %Ecualizacion de la imagen
+    imgec=histeq(img,n);%Número de niveles discretos de gris
+
+    %Mostramos la imagen
+    figure
+    imshowpair(img,imgec,'montage')
+
+    figure
+    subplot(1,2,1)
+    imhist(img)
+    title('Histograma original')
+    subplot(1,2,2)
+    imhist(imgec)
+    title('Histograma Ecualizado')
+end
